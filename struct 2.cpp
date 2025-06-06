16cabd4d28c0de7fc0201cb151756dd59c8e82ff
wyr0o <brykin-2006@mail.ru>
2025-06-06 22:56:30 +0300

    Add files via upload

---
+++ struct 2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct adress
+{
+	std::string city;
+	std::string street;
+	int houseNum;
+	int apartmentNum;
+	int index;
+};
+
+void printAdress(adress& adr)
+{
+	std::cout << "Город: " << adr.city << std::endl;
+	std::cout << "Улица: " << adr.street << std::endl;
+	std::cout<<"Номер дома: " << adr.houseNum << std::endl;
+	std::cout<< "Номер квартиры: " << adr.apartmentNum << std::endl;
+	std::cout << "Индекс: " << adr.index << std::endl;
+	std::cout << std::endl;
+}
+
+int main() 
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	adress adr1 = { "Москва", "Арбат", 12, 54, 8 };
+	adress adr2 = { "Ижевск", "Пушкина", 59, 143, 953769 };
+
+	printAdress(adr1);
+	printAdress(adr2);
+}
