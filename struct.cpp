eca1b749110637ce17ce57cd620f4e629cc2a4ce
wyr0o <brykin-2006@mail.ru>
2025-06-12 01:28:34 +0300

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct bank
+{
+	int AccNum{};
+	std::string FirstName{};
+	float balance{};
+};
+
+void BankNewBalance(bank& account, float& NewBalance)
+{
+	account.balance = NewBalance;
+}
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	bank account;
+	float NewBalance{};
+
+	std::cout << "Введите номер счёта: ";
+	std::cin >> account.AccNum;
+	std::cout << "Введите ваше имя: ";
+	std::cin >> account.FirstName;
+	std::cout << "Введите баланс: ";
+	std::cin >> account.balance;
+	std::cout << "Введите новый Баланс: ";
+	std::cin >> NewBalance;
+
+	BankNewBalance(account, NewBalance);
+	std::cout << "Ваш счёт: " << account.FirstName << ", " << account.AccNum << ", " << account.balance;
+}
